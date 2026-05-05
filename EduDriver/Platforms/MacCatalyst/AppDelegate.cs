d7964b9dde80fe3f8a9462af7992f8fa876a2e54
Arshdeep Singh <erarshdeep@live.com>
2026-05-05 22:13:04 +0400

    fix: add Platforms folder, remove bin/obj from tracking

---
+++ EduDriver/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+using Foundation;
+
+namespace EduDriver;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ EduDriver/Platforms/MacCatalyst/Program.cs
@@ -0,0 +1,11 @@
+using UIKit;
+
+namespace EduDriver;
+
+public class Program
+{
+	static void Main(string[] args)
+	{
+		UIApplication.Main(args, null, typeof(AppDelegate));
+	}
+}
